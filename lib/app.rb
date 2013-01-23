2a5a8e33045cfb62b585a56de41e009c8aee9d18
Justin Balthrop <git@justinbalthrop.com>
2013-01-22 17:57:15 -0800

    clean up application logic and unify hooks into a single hook

--- lib/app.rb
+++ lib/app.rb
@@ -1,3 +1,4 @@
+require 'pp'
 require 'sinatra'
 require 'json'
 require 'faraday'
@@ -9,6 +10,8 @@ require 'issue_comment'
 class App < Sinatra::Base
   enable :sessions
 
+  attr_reader :payload, :repo, :issue, :comment
+
   def oauth
     @oauth ||= Faraday.new(:url => "https://github.com/", :headers => {:accept => 'application/json'}) do |conn|
       conn.request :json
@@ -17,11 +20,14 @@ class App < Sinatra::Base
     end
   end
 
+  def repo_name
+    "#{params[:user]}/#{params[:repo]}"
+  end
+
   get '/issues/:user/:repo' do
     if token = session[:token]
-      @repo = Repo.new("#{params[:user]}/#{params[:repo]}")
+      @repo = Repo.new(repo_name, params)
       @repo.token = token
-      @repo.opts  = params
       erb :index
     else
       url = oauth.build_url("/login/oauth/authorize", {
@@ -46,44 +52,40 @@ class App < Sinatra::Base
   end
 
   def parse_payload
-    @payload = params['payload']
-    @repo    = Repo.new(payload['repository'])
-    @issue   = Issue.new(payload['issue'])
-    @comment = IssueComment.new(payload['comment']) if payload['comment']
-
-    @repo.token = @repo.hook.access_token
+    @payload = JSON.parse(params[:payload])
+    @repo    = Repo.new(@payload['repository'])
+    @issue   = Issue.new(@repo, @payload['issue'])
+    @comment = IssueComment.new(@repo, @payload['comment']) if @payload['comment']
   end
 
-  post '/hooks/issues' do
+  post '/hook' do
     parse_payload
-    action = @payload['action']
+    repo.token = params[:access_token]
 
-    labels = @repo.hook.action_labels(action)
+    if comment
+      if issue.data['closed_at'] == issue.data['updated_at']
+        # Avoid race condition when "Close & comment" is clicked by waiting and
+        # then reloading the issue labels.
+        sleep 5
+        issue.reload!
+      end
 
-    if action == 'opened'
-      labels += @repo.hook.message_labels(@issue.body)
+      issue.update!(:milestone => repo.hook.message_milestone(repo, comment.body))
+      issue.update!(:assignee  => repo.hook.message_assignee(comment.body))
 
-      @issue.update!(:milestone => @repo.hook.message_milestone(@issue.body))
-      @issue.update!(:assignee  => @repo.hook.message_assignee(@issue.body))
+      labels = repo.hook.action_labels(:commented) + repo.hook.message_labels(comment.body)
+      issue.update_labels!(labels)
+    else
+      labels = repo.hook.action_labels(payload['action'])
+
+      if action == 'opened'
+        labels += repo.hook.message_labels(issue.body)
+
+        issue.update!(:milestone => repo.hook.message_milestone(issue.body))
+        issue.update!(:assignee  => repo.hook.message_assignee(issue.body))
+      end
+
+      issue.update_labels!(labels)
     end
-
-    issue.update_labels!(labels)
-  end
-
-  post '/hooks/issue_comment' do
-    parse_payload
-
-    if issue.data['closed_at'] == issue.data['updated_at']
-      # Avoid race condition when "Close & comment" is clicked by waiting and
-      # then reloading the issue labels.
-      sleep 5
-      issue.reload!
-    end
-
-    @issue.update!(:milestone => @repo.hook.message_milestone(@comment.body))
-    @issue.update!(:assignee  => @repo.hook.message_assignee(@comment.body))
-
-    labels = @repo.hook.action_labels(:commented) + @repo.hook.message_labels(@comment.body)
-    @issue.update_labels!(labels)
   end
 end
